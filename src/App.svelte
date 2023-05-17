eb88648cef779ff20619828b72abcacbab504522
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-05-16 18:47:26 -0700

    added axios, updated call to endpoint

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,34 @@
 <script>
-  const handleSubmit = () => {
-    console.log('submitting');
-    fetch('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
-      method: 'POST',
-      item: document.getElementById('item').value
-    })
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById('answer').innerHTML = data.answer;
-      });
+  import axios from 'axios';
+
+  const handleSubmit = async () => {
+    const response = await axios.post('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
+      item: document.getElementById('item-input').value,
+    });
+    
+    document.getElementById('answer').innerHTML = response.data.completion.content;
   }
+
+
+  // const handleSubmit = async () => {
+  //   console.log('submitting');
+  //   console.log(document.getElementById('item-input').value);
+  //   fetch('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
+  //     method: 'POST',
+  //     body: JSON.stringify({ item: document.getElementById('item-input').value }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       document.getElementById('answer').innerHTML = data.completion.content;
+  //     });
+  // };
 </script>
 
 <main class="main-container">
 	<h3>what can you eat when you're</h3>
 	<h1>PREGGO?</h1>
   <div>
-    <input type="text" />
+    <input id="item-input" type="text" />
     <button on:click={handleSubmit}>Submit</button>
   </div>
   <br/>
