08149674c5a18c233e9d94518ff41fd60a1c3c97
ZORDON-PC\Jason <jp_rivera_@hotmail.com>
2023-05-16 19:21:56 -0700

    updated layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,79 @@
 <script>
   import axios from 'axios';
+  import { Circle } from 'svelte-loading-spinners';
+
+  let isLoading = false;
 
   const handleSubmit = async () => {
-    const response = await axios.post('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
-      item: document.getElementById('item-input').value,
-    });
-    
-    document.getElementById('answer').innerHTML = response.data.completion.content;
+    try {
+      isLoading = true;
+      const response = await axios.post('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
+        item: document.getElementById('item-input').value,
+      });
+      document.getElementById('answer').innerHTML = response.data.completion.content;
+    } catch (e) {
+      console.log(e);
+    }
+    isLoading = false;
   }
 
+  const handleEnterPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
+  }
 
-  // const handleSubmit = async () => {
-  //   console.log('submitting');
-  //   console.log(document.getElementById('item-input').value);
-  //   fetch('https://netlify-jason-rivera-serverless-functions.netlify.app/.netlify/functions/api/preggo', {
-  //     method: 'POST',
-  //     body: JSON.stringify({ item: document.getElementById('item-input').value }),
-  //   })
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       document.getElementById('answer').innerHTML = data.completion.content;
-  //     });
-  // };
 </script>
 
 <main class="main-container">
 	<h3>what can you eat when you're</h3>
 	<h1>PREGGO?</h1>
   <div>
-    <input id="item-input" type="text" />
-    <button on:click={handleSubmit}>Submit</button>
+    <input on:keypress={handleEnterPress} id="item-input" type="text" />
   </div>
   <br/>
-  <h4>Answer:</h4>
-  <div id="answer"></div>
+
+  {#if isLoading}
+    <Circle size="35" color="#FF3E00" unit="px" duration="1s"/>
+  {:else}
+    <div id="ask-btn" on:click={handleSubmit} on:keypress={handleSubmit}>Ask</div>
+  {/if}
+
+  <div id="answer" class="answer"></div>
 </main>
 
 <style>
 
 .main-container {
+  margin-top: 100px;
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 20px;
+}
+
+#item-input {
+  text-align: center;
+  padding: 5px;
+  font-size: 1.1rem;
+}
+#ask-btn {
+  background: #FF3E00;
+  border-radius: 50%;
+  padding: 10px;
+  cursor: pointer;
+  transition: 300ms all;
+}
+
+#ask-btn:hover {
+  background: #FF3E00;
+  opacity: 0.8;
+}
+
+.answer {
+  padding-top: 20px;
+  text-align: left;
+  max-width: 500px;
 }
 </style>
